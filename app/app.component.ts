809ee5db1a04ada4c24cf4071186706e0e28f19d
stelalwar <stelalwar@factset.com>
2017-05-07 11:32:32 -0400

    First Angular 2 commit

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'pm-app',
+    template :
+    `<div>
+    <h1> {{pageTitle}} </h1>
+    <div>hii hello</div>
+    </div>`
+})
+export class AppComponent {
+pageTitle : string = 'Gagas Product management'; 
+}
