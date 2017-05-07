124ae1e6bcbbeb18be9784a9e6d08677ab9225c6
stelalwar <stelalwar@factset.com>
2017-05-07 18:43:57 -0400

    Two way binding & Filter

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {ProductListComponent} from './products/product-list.component';
 import { AppComponent }  from './app.component';
-
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule,FormsModule ],
   declarations: [ AppComponent , ProductListComponent],
   bootstrap: [ AppComponent ]
 })
